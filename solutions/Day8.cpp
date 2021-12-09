503b64d578a37fcaabb2f8c926651296b823f474
WarmOverdrive <hankatherton@warmoverdrive.com>
2021-12-09 13:50:26 -0800

    fixed test input, mapped out solution A

--- solutions/Day8.cpp
+++ solutions/Day8.cpp
@@ -2,6 +2,8 @@
 #define DAY_8_CPP
 
 #include <vector>
+#include <tuple>
+#include <array>
 #include <string>
 
 namespace aoc
@@ -10,6 +12,14 @@ namespace aoc
 	{
 		uint64_t Part1(std::vector<std::string> &data)
 		{
+			// segment key:: 1 = 2seg || 4 = 4seg || 7 = 3seg || 8 = 7seg
+
+			// organize into a vector of tuples, of arrays of strings (first (10): input, second (5): output)
+			// seperate function to parse input, will probably be used in part 2
+
+			// for each tuple, check each element in second array for size of 2, 4, 3, 7 and add to tally result
+
+			// return tally
 
 			return 0;
 		}
